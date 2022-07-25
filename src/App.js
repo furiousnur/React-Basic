9b80457e4bb975dd987ee8e884171aad523c3999
furiousnur <furiousnur@gmail.com>
2022-07-26 00:07:11 +0600

    react props

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
       <div>
         <h1 className="headingStyle">Welcome to React Js</h1>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card title="Card 1" detail="If you don’t experience the problems described above or don’t feel comfortable using JavaScript tools yet"/>
+        <Card title="Card 2" detail="If you don’t experience the problems described above or don’t feel comfortable using JavaScript tools yet"/>
+        <Card title="Card 3" detail="If you don’t experience the problems described above or don’t feel comfortable using JavaScript tools yet"/>
       </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,18 @@ const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const yearName = date.getFullYear();
-const title = "Call Family";
-const detail = "If you don’t experience the problems described above or don’t feel comfortable using JavaScript tools yet"
+// const title = "Call Family";
+// const detail = "If you don’t experience the problems described above or don’t feel comfortable using JavaScript tools yet"
 /* end variable declare*/
 /* start variable component function*/
-function Card(){
-    return <div className="card">
-        <h2 className="cardTitle">{title}</h2>
-        <p className="cardDetail">{detail}</p>
-        <p className="cardFooter">{dateName + " " + monthName + " " + yearName }</p>
-    </div>
+function Card(props){
+    return(
+        <div className="card">
+            <h2 className="cardTitle">{props.title}</h2>
+            <p className="cardDetail">{props.detail}</p>
+            <p className="cardFooter">{dateName + " " + monthName + " " + yearName }</p>
+        </div>
+    );
 }
 /* end variable component function*/
 
