0b0ec96edb7dea3439041dbd114ce2ca222eba69
furiousnur <furiousnur@gmail.com>
2022-07-26 00:44:51 +0600

    data mapping instead of forloop

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Data from "./data.json"
 
 function App() {
     let items = [];
-    for (let x = 0; x<Data.length; x++){
-        items.push(<Card title={Data[x].title} detail={Data[x].description} stock={Data[x].stock}/>)
-    }
+    //start for loop
+        // for (let x = 0; x<Data.length; x++){
+        //     items.push(<Card title={Data[x].title} detail={Data[x].description} stock={Data[x].stock}/>)
+        // }
+    //end for loop
+    //start map
+       items = Data.map((item, index) => <Card key={index} titel={item.title} detail={item.description} stock={item.stock}/>)
+    //end map
   return (
       <div>
         <h1 className="headingStyle">Welcome to React Js</h1>
