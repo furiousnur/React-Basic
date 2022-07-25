e02bf22c41952db5b545853eb8df09fa335a1d28
furiousnur <furiousnur@gmail.com>
2022-07-25 23:52:47 +0600

    component create and import in app js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// const headingStyle = {
-//     backgroundColor: "purple",
-//     color:"white",
-//     textAlign: "center",
-//     padding:"15px"
-// }
-
-const date = new Date();
-const dateName = date.getDate();
-const monthName = date.getMonth();
-const yearName = date.getFullYear();
-const title = "Call Family";
-const detail = "If you don’t experience the problems described above or don’t feel comfortable using JavaScript tools yet"
-
 root.render(
-  <React.StrictMode>
-    <div>
-        <h1 className="headingStyle">Welcome to React Js</h1>
-        <div className="card">
-            <h2 className="cardTitle">{title}</h2>
-            <p className="cardDetail">{detail}</p>
-            <p className="cardFooter">{dateName + " " + monthName + " " + yearName }</p>
-        </div>
-    </div>
-  </React.StrictMode>
+    <App/>
 );
