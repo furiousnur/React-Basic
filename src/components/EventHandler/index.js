bdfb2084ed8ef6faaa98cfdda5d22daf99a65d93
furiousnur <furiousnur@gmail.com>
2022-07-30 11:52:04 +0600

    difference between e6 and normal function and binding issue

--- src/components/EventHandler/index.js
+++ src/components/EventHandler/index.js
@@ -8,12 +8,18 @@ class EventHandler extends Component {
         super(props);
 
         this.state = {
-            changedValue : ''
+            changedValue : '',
+            count: 0
         }
+
+        // this.increase = this.increase.bind(this)
     }
 
-    handleClick = () => {
-
+    increase(){
+        //If you have done this by e6 then not needed binding to button. You can also use binding in constructor
+        this.setState({
+            count: this.state.count +1
+        })
     }
 
     handleOnChange = (e) => {
@@ -28,13 +34,13 @@ class EventHandler extends Component {
         return (
             <div>
                 <div className="card">
-                    <h2>EventHandler</h2>
+                    <h2>EventHandler: {this.state.count}</h2>
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Change Value: {this.state.changedValue}</Form.Label>
                             <Form.Control onChange={this.handleOnChange} type="text" placeholder="Enter email" />
                         </Form.Group>
-                        <Button onClick={this.handleClick} variant="primary" type="submit">
+                        <Button onClick={this.increase.bind(this)} variant="primary">
                             Submit
                         </Button>
                     </Form>
