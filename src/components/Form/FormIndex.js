06f72a273fe80bcacea5766fe9807dca4a0ffc0c
furiousnur <furiousnur@gmail.com>
2022-07-30 17:43:55 +0600

    use spread operator for catch all form feilds data

--- src/components/Form/FormIndex.js
+++ src/components/Form/FormIndex.js
@@ -10,16 +10,33 @@ function FormIndex() {
     const [user, setUser] = useState({name: '', email: '', password: ''})
     const {name, email, password} = user
     const submitHandle = (e) =>{
-        let userInfo = {
-            name,
-            email,
-            password
-        }
+        // let userInfo = {
+        //     name,
+        //     email,
+        //     password
+        // }
         // console.log("form submitted")
         console.log(user)
         e.preventDefault();
     }
-    const nameHandle = (e) =>{
+
+    const handleField = (e) => {
+        //spread operator
+        setUser({...user,[e.target.name]: e.target.value});
+        // if (e.target.name === 'name'){
+        //     setUser({name: e.target.value, email, password})
+        // }
+        //
+        // if (e.target.name === 'email'){
+        //     setUser({name, email: e.target.value, password})
+        // }
+        //
+        // if (e.target.name === 'password'){
+        //     setUser({name, email, password: e.target.value})
+        // }
+    }
+
+    /*const nameHandle = (e) =>{
         setUser({name: e.target.value, email, password})
     }
 
@@ -31,7 +48,7 @@ function FormIndex() {
     const passwordHandle = (e) =>{
         // setUser(e.target.value)
         setUser({password: e.target.value, email, name})
-    }
+    }*/
 
     return (
         <div>
@@ -39,17 +56,17 @@ function FormIndex() {
                 <Form onClick={submitHandle}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Name address</Form.Label>
-                        <Form.Control onChange={nameHandle} value={name} type="text" placeholder="Enter name" required/>
+                        <Form.Control onChange={handleField} name="name" value={name} type="text" placeholder="Enter name" required/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control onChange={emailHandle} value={email} type="email" placeholder="Enter email" required/>
+                        <Form.Control onChange={handleField} name="email" value={email} type="email" placeholder="Enter email" required/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control onChange={passwordHandle} value={password} type="password" placeholder="Password" required />
+                        <Form.Control onChange={handleField} name="password" value={password} type="password" placeholder="Password" required />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
