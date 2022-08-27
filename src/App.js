bf6ce600cb72f1b901237d5225ebd2c681124346
Nur <furiousnur@gmail.com>
2022-08-27 14:37:42 +0600

    weather app made using api and state, effect hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CounterView from "./features/counter/counterView"; 
-import PostsView from "./features/posts/PostsView";  
+import PostsView from "./features/posts/PostsView";
+import Temp from "./components/Weather/temp";  
 
 function App() {  
     return (
       <div> 
            {/*<CounterView />*/}
-          <PostsView />
+          {/*<PostsView />*/}
+          <Temp/>
       </div>
   );
 }
