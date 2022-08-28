1d861cc2080eef2a0eac05b81d4d017068e6924c
Nur <furiousnur@gmail.com>
2022-08-29 02:03:56 +0600

    useRef hook use in form, direct access dom using ref

--- src/components/Lazy/Other.js
+++ src/components/Lazy/Other.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const Other = () => {
+    const userNameRef = useRef();
+    const passwordRef = useRef();
+    
+    const handleSubmitForm = (e) =>{
+        e.preventDefault();
+        const name = userNameRef.current.value;
+        const password = passwordRef.current.value;
+        console.log({name, password});
+    }
     return (
         <div>
             <h1>This is Other component</h1>
+            <form onSubmit={handleSubmitForm}>
+                <input type="text" name="userName" ref={userNameRef}/>
+                <input type="password" name="password" ref={passwordRef}/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
