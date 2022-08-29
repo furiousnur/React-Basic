bcc472ffb7189d455553692a9e617debc84babc6
Nur <furiousnur@gmail.com>
2022-08-30 00:01:45 +0600

    lazy, suspense with useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import RefExample from "./components/RefExample/RefExample"; 
+import React, {Suspense} from 'react'; 
+const RefExample = React.lazy(()=> import('../src/components/RefExample/RefExample'));
 
 function App() {  
     return (
       <div>
-          <RefExample/>
+          <Suspense fallback={<div>Loading...</div>}>
+              <RefExample/>
+          </Suspense> 
       </div>
   );
 }
